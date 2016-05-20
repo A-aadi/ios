a0582413722357aea161b6cd47909958c427a47f
adarshks <adarshks@inblr-adarshks.local>
2016-05-20 11:34:15 +0530

    added a join page

---
+++ CallBreak/JoinViewController.h
@@ -0,0 +1,15 @@
+//
+//  JoinViewController.h
+//  CallBreak
+//
+//  Created by adarshks on 5/19/16.
+//  Copyright © 2016 adarshks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JoinViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *myTextView;
+@property (weak, nonatomic) IBOutlet UIButton *joinButton;
+
+@end
---
+++ CallBreak/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  CallBreak
+//
+//  Created by adarshks on 5/19/16.
+//  Copyright © 2016 adarshks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
